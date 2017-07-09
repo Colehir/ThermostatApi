1f13df8a7a34e8ac145a2f8661435cc3468d37ec
Colehir <cole.hirapara@gmail.com>
2017-07-09 16:31:18 -0400

    Add project files.

---
+++ ThermostatApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ThermostatApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ThermostatApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThermostatApi.WebApiApplication" Language="C#" %>
